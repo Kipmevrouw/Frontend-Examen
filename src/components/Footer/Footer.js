e52a5b70ded67cc4d32291a709e81b5704f3fbd8
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2022-12-20 11:30:55 +0100

    footer voor nu responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,14 +6,14 @@ const Footer = (props) => {
     return(
         <section className="footer__section">
             <header className="footer__header">
-                <div className="footer__div">
+                <div className="footer__div div1">
                     <h1>Naviagatie</h1>
                     <p>Home</p>
                     <p>Over</p>
                     <p>Features</p>
                     <p>Contact</p>
                 </div>
-                <div className="footer__div">
+                <div className="footer__div div">
                     <h1>Support</h1>
                     <p>Bel ons via 020-0354834</p>
                     <p>pokemoncards@help.nl</p>
